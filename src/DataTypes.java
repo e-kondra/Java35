fcff3c31c325abb2dd40e2e039e542df106209fb
e-kondra <e-kondra@mail.ru>
2023-02-10 21:33:47 +0200

    DataTypes and variables

---
+++ src/DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Integer Data types
+        byte numOne = 100;
+        short numTwo = 1000;
+        int numThree = 345253;
+        long numFour = 4123556221L;
+
+        //Floating point
+        float numFive = 123456.23655f;
+        double numSix = 2123.23233655454d;
+
+        //Boolean
+        boolean redLightSwitch = true;
+        boolean blackLightSwitch = false;
+
+        //Character
+        char charOne = '%';
+        char charTwo = 'x';
+        char charThree = '0';
+
+        //String
+        String textOne = "It's a good text";
+        //print them out
+        System.out.println(charThree);
+        System.out.println(redLightSwitch);
+        System.out.println(textOne);
+        System.out.println(numSix);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declaring a new variable of type int
+        int number;
+        //Initialization
+        number = 10;
+        //Accessing number variables value
+        System.out.println( number);
+
+        int age = 75;
+        System.out.println("age = "+age);
+    }
+}
