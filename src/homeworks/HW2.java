5df1ac22034b1b2c9a410d7cd7b60acac9f0774f
e-kondra <e-kondra@mail.ru>
2023-02-18 12:52:47 +0200

    H2

--- src/homeworks/HW2.java
+++ src/homeworks/HW2.java
@@ -8,6 +8,7 @@ public class HW2 {
         // Calculate and output speed in km/h
         // Example with input 122.7 mp/h
         // Output --> 122.7 mp/h in km/h would be equal to 197.46651
+
         final float convert = 1.60934399f;
 
         Scanner scanner = new Scanner(System.in);
@@ -19,10 +20,5 @@ public class HW2 {
         } else {
             System.out.println("You've written a wrong number");
         }
-
-
-
-
-
     }
 }
