b028d96832110c71631d4ef7a0c4ff1dc487658d
e-kondra <e-kondra@mail.ru>
2023-02-20 21:27:14 +0200

    Switch and Loops

---
+++ src/basics/Loops.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + ". Hello world!");
+        }
+        for(int n = 0; n<=100; n += 10){
+            System.out.print(n + " ");
+        }
+    }
+}
